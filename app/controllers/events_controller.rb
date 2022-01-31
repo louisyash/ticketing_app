2b7aeda0cfa26a244f6df9d66f8eb73df3d3677b
Louis Yasuhiro <lyasuhir@gmail.com>
2022-01-31 18:15:19 +0900

    done with implementation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   def index
+    @events = policy_scope(Event).order(created_at: :desc)
     params[:query].present? ? @events = Event.where(name: params[:query]) : @events = Event.all
 
     @markers = @events.geocoded.map do |event|
@@ -12,6 +13,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    authorize @event
   end
 
 #   private
